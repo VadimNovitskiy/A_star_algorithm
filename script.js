57677eb604bb7de387528a2b52d5c11cfa2b2d2e
Vadim <vadim.novitskiy.98@gmail.com>
2022-01-21 15:26:12 +0200

    clear loop in show

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let cols = 5;
-let rows = 5;
-let width = 400;
-let height = 400;
+const startBtn = document.querySelector('#start');
+const resetBtn = document.querySelector('#reset');
+
+let state;
+let cols = 50;
+let rows = 50;
+let width = 800;
+let height = 800;
 const grid = new Array(cols);
 let canvas = new fabric.Canvas('canvas', {
     width: width,
@@ -19,20 +23,6 @@ let path = [];
 let winner;
 let current;
 
-function heuristic(a, b) {
-    // let d = Math.hypot(a.i, a.j, b.i, b.j);
-    let d = Math.abs(a.i - b.i) + Math.abs(a.j - b.j)
-    return d;
-}
-
-function removeFromArray(arr, elem) {
-    for(let i = arr.length - 1; i >= 0; i--) {
-        if(arr[i] == elem) {
-            arr.splice(i, 1)
-        }
-    }
-}
-
 class Spot{
     constructor(i, j) {
         this.i = i;
@@ -44,23 +34,35 @@ class Spot{
 
         this.neighbors = [];
         this.previous = undefined;
+        this.wall = false;
     }
 
     show(col) {
         let rect = new fabric.Rect({
-            stroke: '#fff',
+            // stroke: '#fff',
             top: this.i * w, 
             left: this.j * h,
             width: w - 1,
             height: h - 1,
-        })
-        canvas.add(rect);
-        rect.set({fill: col});
+            hoverCursor: "pointer",
+        });
+
+        rect.set({fill: col})
+
+        if(this.wall) {
+            rect.set({fill: '#00000'})
+        }
 
         rect.hasBorders = false;
         rect.hasControls = false;
         rect.lockMovementX = true;
         rect.lockMovementY = true;
+        rect.subTargetCheck =  true;
+        canvas.add(rect);
+    }
+
+    static render() {
+        canvas.renderAll();
     }
 
     addNeighbors(grid) {
@@ -77,6 +79,54 @@ class Spot{
         }
         if(j > 0) {
             this.neighbors.push(grid[i][j - 1]);
+        }
+        if (i > 0 && j > 0) {
+            this.neighbors.push(grid[i - 1][j - 1]);
+        }
+        if (i < cols - 1 && j > 0) {
+            this.neighbors.push(grid[i + 1][j - 1]);
+        }
+        if (i > 0 && j < rows - 1) {
+            this.neighbors.push(grid[i - 1][j + 1]);
+        }
+        // if (i < cols - 1 && j < rows - 1) {
+        //     this.neighbors.push(grid[i + 1][j + 1]);
+        // }
+    }
+}
+
+function startAlg() {
+    setup();
+    startBtn.addEventListener('click', draw);
+    resetBtn.addEventListener('click', reset);
+}
+startAlg();
+
+function reset() {
+    console.log('reset');
+    removeRect();
+    openSet.length = 0;
+    closedSet.length = 0;
+    path.length = 0;
+    grid.length = 0;
+    state = 'start';
+    startAlg();
+}
+
+function removeRect() {
+    canvas.clear()
+}
+
+function heuristic(a, b) {
+    // let d = Math.hypot(a.i, a.j, b.i, b.j);
+    let d = Math.abs(a.i - b.i) + Math.abs(a.j - b.j)
+    return d;
+}
+
+function removeFromArray(arr, elem) {
+    for(let i = arr.length - 1; i >= 0; i--) {
+        if(arr[i] == elem) {
+            arr.splice(i, 1)
         }
     }
 }
@@ -101,184 +151,108 @@ function setup() {
             grid[i][j].addNeighbors(grid);
         }
     }
+    
+    for(let i = 0; i < 700; i++) {
+        let items = grid[Math.floor(Math.random() * cols)][Math.floor(Math.random() * rows)];
+        items.wall = true;
+    }
 
     start = grid[0][0];
-    end = grid[cols - 1][rows - 1];
+    end = grid[cols - 1][rows -1];
+    start.wall = false;
+    end.wall = false;
 
     openSet.push(start);
-    draw();
-}
-
-function rendering() {
 
     for(let i = 0; i < cols; i++) {
         for(let j = 0; j < rows; j++) {
-            grid[i][j].show();
+            grid[i][j].show('#fff');
         }
     }
-    
-    for(let i = 0; i < openSet.length; i++) {
-        openSet[i].show('#72f738');  /// Green
-    }
-
-    for(let i = 0; i < closedSet.length; i++) {
-        closedSet[i].show('#ff6425'); /// Red
-    }
-
-    let temp = current;
-    path.push(temp);
-    while(temp.previous) {
-        path.push(temp.previous)
-        temp = temp.previous;
-        console.log(path);
-    }
-    
-    for(let i = 0; i < path.length; i++) {
-        path[i].show('#4fbcf2');  /// Blue
-    }
-    canvas.renderAll();
 }
 
+async function draw() {
+    startBtn.removeEventListener('click', draw);
 
-function draw() {
-    // console.log(openSet);
-    let intervalId = setInterval(() => {
-        if(openSet.length > 0) {
+    for(let iteration of algo()){
 
-            winner = 0;
-            for(let i = 0; i < openSet.length; i++) {
-                if(openSet[i].f < openSet[winner].f) {
-                    winner = i;
-                }
+        const {openSet, closedSet, path} = iteration;
+
+        await new Promise(resolve => setTimeout(resolve, 1));
+
+        openSet.at(-1).show('#72f738');  /// Green
+        closedSet.at(-1).show('#ff6425'); /// Red
+    }
+    console.log(path.length);
+
+    for(let i = 0; i < path.length; i++) {
+        path[i].show('#4fbcf2'); /// Blue
+    }
+    return;
+}
+
+function* algo() {
+    console.log('start algorithm');
+
+    while(openSet.length > 0) {
+        winner = 0;
+        for(let i = 0; i < openSet.length; i++) {
+            if(openSet[i].f < openSet[winner].f) {
+                winner = i;
             }
-            current = openSet[winner];
-            // console.log(current);
-    
-            if(current === end) {
-                console.log('Done');
-                clearInterval(intervalId);
-                return;
+        }
+        current = openSet[winner];
+
+        if(current === end) {
+            console.log('Done');
+            let temp = current;
+            path.push(temp);
+            while (temp.previous) {
+                path.push(temp.previous);
+                temp = temp.previous;
             }
-    
-            removeFromArray(openSet, current)
-            closedSet.push(current);
-            // console.log(openSet);
-            // console.log(closedSet);
-    
-            let neighbors = current.neighbors;
-    
-            for(let i = 0; i < neighbors.length; i++) {
-                let neighbor = neighbors[i];
-    
-                if(!closedSet.includes(neighbor)) {
-                    let tempG = current.g + 1;
-    
-                    let newPath = false;
-                    if(openSet.includes(neighbor)){
-                        if(tempG < neighbor.g) {
-                            neighbor.g = tempG;
-                            newPath = true;
-                        } 
-                    } else {
+            break;
+        }
+
+        removeFromArray(openSet, current)
+        closedSet.push(current);
+        
+
+        let neighbors = current.neighbors;
+
+        for(let i = 0; i < neighbors.length; i++) {
+            let neighbor = neighbors[i];
+
+            if(!closedSet.includes(neighbor) && !neighbor.wall) {
+
+                let tempG = current.g + 1;
+                let newPath = false;
+                if(openSet.includes(neighbor)){
+                    if(tempG < neighbor.g) {
                         neighbor.g = tempG;
                         newPath = true;
-                        openSet.push(neighbor);
-                    }
-    
-                    if(newPath) {
-                        neighbor.h = heuristic(neighbor, end);
-                        neighbor.f = neighbor.g + neighbor.h;
-                        neighbor.previous = current;
-                    }
+                    } 
+                } else {
+                    neighbor.g = tempG;
+                    newPath = true;
+                    openSet.push(neighbor);
+                }
+
+                if(newPath) {
+                    neighbor.h = heuristic(neighbor, end);
+                    neighbor.f = neighbor.g + neighbor.h;
+                    neighbor.previous = current;
                 }
             }
-            rendering();
         }
-    }, 1000) 
-        // console.log('no solution');
-        // return;
+
+        yield {
+            openSet,
+            closedSet,
+            path,
+            current,
+            winner,
+        };
+    }
+    console.log('end algorithm');
 }
-setup()
-// onChange: canvas.renderAll.bind(canvas)
-// https://habr.com/ru/post/167119/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function createCanvas() {
-//     const canvas = new fabric.Canvas('canvas', {
-//         width: 501,
-//         height: 501,
-//         backgroundColor: '#181a1b',
-//     });
-    
-//     for(let i = 0; i < 25; i++) {
-//         for(let j = 0; j < 25; j++) {
-//             let rect = new fabric.Rect({
-//                 left: j*20,
-//                 top: i*20,
-//                 width: 20, 
-//                 height: 20, 
-//                 stroke: 'rgba(255,255,255,0.5)',
-//             });
-//             canvas.add(rect);
-//             rect.hasBorders = false;
-//             rect.hasControls = false;
-//             rect.lockMovementX = true;
-//             rect.lockMovementY = true;
-//             addColor(rect);
-//         }
-//     }
-// }
-
-// function addColor(rect) {
-//     let group = new fabric.Group([rect], {
-//         subTargetCheck: true
-//     });
-    
-//     rect.on('mousedown', (event) => {
-//         let targetRect = event.target;
-//         targetRect.set({fill: 'red'})
-//     })
-// } 
-
-
-// function start() {
-//     switch(state) {
-//         case 'init':
-//             createCanvas();
-//             break;
-//     }
-// }
-// start();
